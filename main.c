19b5c7b2e5ca7bfb6ed4fdc9f25a4f02672bff13
boonan <paigemartincpaz@gmail.com>
2021-04-06 20:54:48 -0400

    interrupts with pushbutton. currently outputs whatever button is pressed to the 7-seg display

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include "address_map_arm.h"
 #include "vga.h"
+#include "interrupts.h"
 
 int main(void) {
+    //disable_A9_interrupts();
+    set_A9_IRQ_stack();
+    config_GIC();
+    config_KEYs();
+    enable_A9_interrupts();
     vga_init();
     vga_set_screen(BLACK);
     vga_show_screen();
@@ -9,5 +15,11 @@ int main(void) {
     for(i = 0; i < VGA_WIDTH_BINS*VGA_HEIGHT_BINS; i++) {
         vga_set_segment(i, RED);
     }
+    
+
+    while(1){
+        ;
+    }
+
     return 0;
 }
